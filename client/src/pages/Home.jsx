b2338547208edb1dae81b72a29ba8f2a6d4fee92
Sanjana <sanjana3010terdalkar@gmail.com>
2026-06-24 02:15:17 +0530

    initial commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import react from 'react';
+import HeroSection from '../components/HeroSection';
+import FeatureSection from '../components/FeatureSection';
+import TrailersSection from '../components/TrailersSection';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection/>
+      <FeatureSection/>
+      <TrailersSection/>
+    </div>
+  );
+}
+
+export default Home;
