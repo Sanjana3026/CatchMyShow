94819776cd8118f97df2aac2679978e069fff700
Sanjana <sanjana3010terdalkar@gmail.com>
2026-07-12 01:36:41 +0530

    inngest debug 2

--- server/server.js
+++ server/server.js
@@ -19,14 +19,14 @@ await connectDB();
 //stripe webhooks route
 app.use('/api/stripe', express.raw({type: 'application/json'}) , stripeWebhooks)
 
-//inngest
-app.use("/api/inngest", serve({ client: inngest, functions, }));
-
 //Middleware
 
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
+
+//inngest
+app.use("/api/inngest", serve({ client: inngest, functions }));  
 
 //API Routes 
 app.get('/', (req,res) => res.send('server is live'))
