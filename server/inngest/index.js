0669173f5228a3a31b5c7dbb6e6b8d52e6274af8
Sanjana <sanjana3010terdalkar@gmail.com>
2026-07-12 02:48:30 +0530

    nodemailer

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -123,7 +123,7 @@ const sendBookingConfirmationEmail = inngest.createFunction(
       .populate("user");
     
       await sendEmail({
-      to:  .user.email,
+      to: task.user.email,
       subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
       body: `<div style="font-family: Arial, sans-serif; line-height: 1.5;">
         <h2>Hi ${booking.user.name},</h2>
